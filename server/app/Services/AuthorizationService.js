ce7fc440ad6917f18a2cd8a1e7dec1bbbe548038
Ben <ncor89b@gmail.com>
2022-05-28 01:08:57 +0200

    Customization of the delete Route
    add  exception for resource access

--- server/app/Services/AuthorizationService.js
+++ server/app/Services/AuthorizationService.js
@@ -1,10 +1,16 @@
+const InvalidAccessException = use('App/Exceptions/InvalidAccessException');
+const ResourceNotExistException = use('App/Exceptions/ResourceNotExistException');
 
 class AuthorizationService {
   verifyPermission(ressource, user) {
+    if(!ressource) {
+      throw new ResourceNotExistException();
+    }
+
     if(ressource.user_id !== user.id) {
-      throw new Error(); //: todo: invalidaccess exeption;
+      throw new InvalidAccessException();
     }
   }
 }
 
-module.exports = AuthorizationService;
+module.exports = new AuthorizationService();

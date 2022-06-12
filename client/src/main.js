2957d16ce362a590efed34d18e5d46169f0ec040
Ben <ncor89b@gmail.com>
2022-06-12 23:59:31 +0200

    awaiting correction

--- client/src/main.js
+++ client/src/main.js
@@ -1,13 +1,13 @@
-import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
-import Vue from 'vue';
-import Vuetify from 'vuetify';
+// import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
+// import Vue from 'vue';
+// import Vuetify from 'vuetify';
 
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
-Vue.use(Vuetify);
+// Vue.use(Vuetify);
 // import { sync } from 'vuex-router-sync';
 // index.js or main.js
 createApp(App).use(store).use(router).mount('#app');
